0ef9267a6790a5df8786593a96bf1f9a24e9005b
Imanuel Harizi <iManuel2K@protonmail.com>
2022-09-10 15:57:20 +0200

    CRSystem started

--- www/site.php
+++ www/site.php
@@ -1,61 +1,41 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>CRSystem</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
 
-echo "this is an echo statement <br>";
+    <form action="site.php" method="post" class="form-inputs">
+        What is your name? <input type="text" name="name" />
+        What is your gender? Male <input type="radio" name="gender-m"> Female <input type="radio" name="gender-f">
+        What is your age? <input type="number" name="age">
+        What is your height? <input type="number" name="height">
+        What is your weight? <input type="number" name="weight">
+        What is your favourite season? <select>
+            <option value="spring" name="spring">Spring</option>
+            <option value="summer" name="summer">Summer</option>
+            <option value="autumn" name="autumn">Autumn</option>
+            <option value="winter" name="winter">Winter</option>
+        </select>
+        <input type="submit">
+    </form>
 
-print "this is a print statement <br>";
+    <?php
 
-$books = array("GOT", "LOTR", "Bible", "Clean Code");
-echo implode(" ", $books);
+    echo $_POST['name'];
+    echo $_POST['gender-f'];
+    echo $_POST['gender-m'];
+    echo $_POST['age'];
+    echo $_POST['height'];
+    echo $_POST['weight'];
+    echo $_POST['spring'];
+    echo $_POST['summer'];
+    echo $_POST['autumn'];
+    echo $_POST['winter'];
 
-echo "<br>";
 
-$name = "Imanuel the 23th";
-$nameArr = explode(" ", $name);
-print_r($nameArr);
-
-$x2 = null;
-var_dump($x2);
-
-echo "<br>";
-
-define("KEYBOARD", "This is a constant! And also a keyboard");
-echo KEYBOARD;
-
-echo "<br>";
-echo "Is Keyboard defined? =>", defined("KEYBOARD");
-
-echo "<br>";
-const KEYBOARD2 = "Another const. And another keyboard!";
-echo KEYBOARD2;
-
-echo "<br>";
-$number = 3;
-define("KEYBOARD" . $number, "Yet another const...");
-echo KEYBOARD3;
-
-echo "<br>";
-echo "Preefined const: ", PHP_INT_MIN;
-echo "<br>";
-echo PHP_VERSION;
-
-//Magic Constants, change automatically based on where they are
-echo __LINE__;
-echo "<br>";
-echo __DIR__;
-
-echo "<br>";
-$sports = ["name" => "Lacrosse", "year_invented" => 1634];
-echo $sports["name"];
-echo "<br>";
-
-// Variable Variables
-$foo = "bar";
-$$foo = "baz";
-
-echo "$foo, {$$foo}";
-echo "<br>";
-
-//Type Casting
-$x = (int) 5;
-echo $x;
-?>
+    ?>
+</body>
+</html>
