1f89a84fe5cb85464899876bfc377d8369a5b1e3
Imanuel Harizi <iManuel2K@protonmail.com>
2022-09-12 21:25:26 +0200

    extended db.php

--- www/db.php
+++ www/db.php
@@ -1,1 +1,40 @@
 <?php
+    $dbServerName = "localhost";
+    $dbUserName = "root";
+    $dbPassword = "";
+    $dbName = "loginapp";
+
+    $conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
+
+    if ($conn) {
+        echo "Connected";
+    } else {
+        die("DB connection failed");
+    }
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+    <title>Databases</title>
+</head>
+    <body>
+
+        <?php
+            $query = "SELECT * FROM users";
+            $result = mysqli_query($conn, $query);
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                print_r($row);
+            }
+        ?>
+
+    </body>
+</html>
